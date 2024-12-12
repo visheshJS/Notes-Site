4d52c4f75243631368d3521f0017d6c993f725a5
visheshJS <visheshsharma_23me287@dtu.ac.in>
2024-12-13 01:27:41 +0530

    just memory fix left

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-const notesContainer =document.querySelector(".notes-container");
-const createBtn =document.querySelector(".btn");
-// const deleteBtn =document.querySelector(".DELETE");
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
 
-let notes =document.querySelectorAll(".input-box");
+let notes = document.querySelectorAll(".input-box");
 
-function createNote(){
-    let inputbox =document.createElement("p");
-    let deleteBtn =document.createElement("button");
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes");
+}
+showNotes();
 
-    inputbox.classList.add("input-box");
-    inputbox.setAttribute("contenteditable", "true");
-    inputbox.innerHTML="write your content here";
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
-    deleteBtn.classList.add("DELETE");
-    deleteBtn.innerHTML='<img src="images/delete.png" alt="delete">';
-    notesContainer.appendChild(inputbox);
-    inputbox.appendChild(deleteBtn);
-    saveData();
+function createNote() {
+  let inputbox = document.createElement("p");
+  let img = document.createElement("img")
+//   let deleteBtn = document.createElement("button");
 
-    deleteBtn.addEventListener("click",() => {
-        notesContainer.removeChild(inputbox);
-        saveData();
-    
-    
+
+  inputbox.classList.add("input-box");
+  inputbox.setAttribute("contenteditable", "true");
+//   inputbox.innerHTML = "write your content here";
+  img.src="images/delete.png"
+
+//   deleteBtn.classList.add("DELETE");
+//   deleteBtn.innerHTML = '<img src="images/delete.png" alt="delete">';
+  notesContainer.appendChild(inputbox).appendChild(img);
+
+  
+}
+notesContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+      e.target.parentElement.remove();
+      updateStorage();
       
-    });
-}
-function saveData(){
-    
-}
-
+    }
+    else if(e.target.tagName==="P"){
+        notes.forEach((nt) => {
+            nt.onkeyup=function(){
+                updateStorage();
+            }
+          
+        })
+    }
+});
 
