0a87cab14e148166610766632977081449e71ed5
visheshJS <visheshsharma_23me287@dtu.ac.in>
2024-12-13 01:55:07 +0530

    done storage management

--- script.js
+++ script.js
@@ -3,29 +3,30 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
-function showNotes(){
-    notesContainer.innerHTML=localStorage.getItem("notes");
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("content" || "");
 }
 showNotes();
 
 function updateStorage() {
-  localStorage.setItem("notes", notesContainer.innerHTML);
+  localStorage.setItem("content", notesContainer.innerHTML);
 }
 
 function createNote() {
   let inputbox = document.createElement("p");
-  let img = document.createElement("img")
-//   let deleteBtn = document.createElement("button");
-
+  let img = document.createElement("img");
+  //   let deleteBtn = document.createElement("button");
 
   inputbox.classList.add("input-box");
   inputbox.setAttribute("contenteditable", "true");
-//   inputbox.innerHTML = "write your content here";
-  img.src="images/delete.png"
+  //   inputbox.innerHTML = "write your content here";
+  img.src = "images/delete.png";
 
-//   deleteBtn.classList.add("DELETE");
-//   deleteBtn.innerHTML = '<img src="images/delete.png" alt="delete">';
+  //   deleteBtn.classList.add("DELETE");
+  //   deleteBtn.innerHTML = '<img src="images/delete.png" alt="delete">';
   notesContainer.appendChild(inputbox).appendChild(img);
+
+  inputbox.addEventListener("keyup", updateStorage);
 
   
 }
@@ -33,15 +34,7 @@ notesContainer.addEventListener("click", (e) => {
     if (e.target.tagName === "IMG") {
       e.target.parentElement.remove();
       updateStorage();
-      
-    }
-    else if(e.target.tagName==="P"){
-        notes.forEach((nt) => {
-            nt.onkeyup=function(){
-                updateStorage();
-            }
-          
-        })
-    }
-});
 
+    }
+
+});
